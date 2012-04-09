11eeca3350794b900f28f8586b02e8c318f540bc
Gin In Networking <ginisnetworking@gmail.com>
2012-04-09 18:39:15 +0100

    Remove concurrentlua

--- src/lua/modules/concurrentlua/test/distributed/process2b.lua
+++
@@ -1,9 +0,0 @@
-require 'concurrent'
-
-concurrent.init('caller@localhost')
-
-pid = concurrent.spawn('remote@localhost', 'pong', 3)
-concurrent.spawn('remote@localhost', 'ping', pid)
-
-concurrent.loop()
-concurrent.shutdown()
--- src/lua/modules/concurrentlua/test/distributed3b.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for i in register2b monitor2b link2b trapexit2b
-	do sleep 2
-	echo running $i.lua
-	lua distributed/$i.lua
-	echo
-done
