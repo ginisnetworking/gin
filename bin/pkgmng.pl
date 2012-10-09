ca6bd90c7d6f44037415a299a99c6818080eec7a
Gin In Networking <ginisnetworking@gmail.com>
2012-10-09 18:32:03 +0100

    adding latest version zmq bindings

--- bin/pkgmng.pl
+++ bin/pkgmng.pl
@@ -43,7 +43,7 @@ my $pkgs   = [
     get     => "curl -C - http://lua.sqlite.org/index.cgi/zip/lsqlite3_svn08.zip?uuid=svn_8 -o lsqlite3_svn08.zip",
     extract => "unzip $pkgdir/lsqlite3_svn08.zip; mv lsqlite3_svn08/* .; rmdir lsqlite3_svn08"},
   { name    => "luazmq", file => "lua-zmq.tar.gz", dest => "lua/modules/lua-zmq",
-    get     => "curl -L -C - https://github.com/iamaleksey/lua-zmq/tarball/master -o lua-zmq.tar.gz",
+    get     => "curl -L -C - https://github.com/Neopallium/lua-zmq/tarball/master -o lua-zmq.tar.gz",
     extract => "tar zxvf $pkgdir/lua-zmq.tar.gz --strip-components 1"},
   { name    => "lcrypt", file => "lcrypt.tgz", dest => "lua/modules/lcrypt",
     get     => "curl -C - -O http://eder.us/projects/lcrypt/lcrypt.tgz",
