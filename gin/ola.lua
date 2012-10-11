be57dcbd79e0bef0322476974492af208eb9d4e7
Gin In Networking <ginisnetworking@gmail.com>
2012-10-11 16:20:07 +0100

    some more problems with stdlib

--- gin/ola.lua
+++ gin/ola.lua
@@ -1,6 +1,13 @@
 
 require "lsqlite3";
 
-print package.path;
+-- print package.path;
 -- print package.loaders[0];
 
+function t(a) if (a) == 0 then return 0 else return a + t(a-1) end end
+
+print( t(100))
+
+--function p (n) print (n) p(n+1) end
+
+--p(1)
